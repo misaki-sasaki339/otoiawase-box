f6c0305b962a8a59a34916150cb1e589089e26a5
misaki sasaki <misaki.morinaga03@gmail.com>
2025-08-20 00:43:31 +0900

    基本的なバリデーションを設定

---
+++ src/app/Http/Requests/ContactRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name'=>'required',
+            'last_name'=>'required',
+            'gender'=>'required',
+            'email'=>'required',
+            'tel'=>'required',
+            'address'=>'required',
+            'detail'=>'required'
+        ];
+    }
+
+    //エラー時の出力を記述
+}
